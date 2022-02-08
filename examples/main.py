92f48eb6cce8124e04990cdee46e41f600691807
guz1guz <99261048+guz1guz@users.noreply.github.com>
2022-02-08 15:10:05 +0200

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,3 +1,12 @@
 """Your main code goes here!"""
 
-print("Guzu")
+import machine
+import time
+
+led = machine.Pin(2,machine.Pin.OUT)
+
+while True:
+  led.value(1)
+  time.sleep(1)
+  led.value(0)
+  time.sleep(1)
